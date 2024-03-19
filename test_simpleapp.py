8412c9f6b4ec865c6d60d56802146ff6f945ec6a
ankuronlyme <144051650+ankuronlyme@users.noreply.github.com>
2024-03-19 13:57:38 +0530

    Update test_simpleapp.py

--- test_simpleapp.py
+++ test_simpleapp.py
@@ -15,13 +15,3 @@ def test_index(client):
 
 
 
-
-
-#import pytest
-#from app import Hello, World! My Jenkins CI/CD pipeline for flask application.
-
-#def test_Hello, World! My Jenkins CI/CD pipeline for flask application.():
- #   assert Hello, World! My Jenkins CI/CD pipeline for flask application.() == 'Hello, World! My Jenkins CI/CD pipeline for flask application.'
-
-
-
