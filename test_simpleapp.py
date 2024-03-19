83ecaf73aba70cc69b91288ada8b69337ff0e59e
ankuronlyme <144051650+ankuronlyme@users.noreply.github.com>
2024-03-19 14:11:11 +0530

    Update test_simpleapp.py

--- test_simpleapp.py
+++ test_simpleapp.py
@@ -7,7 +7,7 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_index(client):
+def test_hello(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b'Hello, World! My Jenkins CI/CD pipeline for flask application.' in response.data
