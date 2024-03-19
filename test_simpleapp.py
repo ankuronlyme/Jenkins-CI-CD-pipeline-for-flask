82cfc94fa09dfef4961522e519db09dd7aaf04fa
ankuronlyme <144051650+ankuronlyme@users.noreply.github.com>
2024-03-19 11:21:29 +0530

    Create test_simpleapp.py

---
+++ test_simpleapp.py
@@ -0,0 +1,8 @@
+import pytest
+from app import Hello, World! My Jenkins CI/CD pipeline for flask application.
+
+def test_Hello, World! My Jenkins CI/CD pipeline for flask application.():
+    assert Hello, World! My Jenkins CI/CD pipeline for flask application.() == 'Hello, World! My Jenkins CI/CD pipeline for flask application.'
+
+
+
